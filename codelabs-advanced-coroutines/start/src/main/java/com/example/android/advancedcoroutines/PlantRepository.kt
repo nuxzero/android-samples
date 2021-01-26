d86212edb49d527809deff9f8182f4060b14e1e4
Natthawut Haematulin <natthawut1991@gmail.com>
2021-01-26 21:39:58 +0700

    feat: complete step 7

--- codelabs-advanced-coroutines/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ codelabs-advanced-coroutines/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -16,13 +16,16 @@
 
 package com.example.android.advancedcoroutines
 
+import androidx.annotation.AnyThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
 import com.example.android.advancedcoroutines.util.CacheOnSuccess
 import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 /**
  * Repository module for handling data operations.
@@ -61,9 +64,13 @@ class PlantRepository private constructor(
      * Returns a LiveData-wrapped List of Plants.
      */
     fun getPlantsWithGrowZone(growZone: GrowZone): LiveData<List<Plant>> = liveData {
-        val plantsGrowZonwLiveData = plantDao.getPlantsWithGrowZoneNumber(growZone.number)
-        val customSortOrder = plantsListSortOrderCache.getOrAwait()
-        emitSource(plantsGrowZonwLiveData.map { plantList -> plantList.applySort(customSortOrder) })
+        plantDao.getPlantsWithGrowZoneNumber(growZone.number)
+            .switchMap { plantList ->
+                liveData {
+                    val customSortOrder = plantsListSortOrderCache.getOrAwait()
+                    emit(plantList.applySort(customSortOrder))
+                }
+            }
     }
 
     /**
@@ -118,6 +125,12 @@ class PlantRepository private constructor(
             ComparablePair(positionForItem, plant.name)
         }
     }
+
+    @AnyThread
+    suspend fun List<Plant>.applyMainSafeSort(customSortOrder: List<String>) =
+        withContext(defaultDispatcher) {
+            this@applyMainSafeSort.applySort(customSortOrder)
+        }
 
     companion object {
 
