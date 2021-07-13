672aaab4a422e563a6639333ce0199fc44d083f0
Natthawut Haematulin <natthawut1991@gmail.com>
2021-07-13 22:11:17 +0700

    feat: create simple form

--- compose-inputs/app/src/main/java/com/example/app/MainActivity.kt
+++ compose-inputs/app/src/main/java/com/example/app/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import com.example.app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,25 +26,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    CreditCardForm()
                 }
             }
-            val navController = rememberNavController()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreditCardForm() {
+    Column() {
+        TextField("Email")
+        TextField("Card Number")
+        Row {
+            TextField("MM/YY")
+            TextField("CVC")
+        }
+        TextField("Name")
+        TextField("Country")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Pay €55.00")
+        }
+    }
+}
+
+@Composable
+fun TextField(placeholder: String = "") {
+    var cardNumber: TextFieldValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue("", TextRange(16)))
+    }
+
+    OutlinedTextField(
+        value = cardNumber,
+        onValueChange = { cardNumber = it },
+        placeholder = { Text(text = placeholder) },
+        shape = MaterialTheme.shapes.medium,
+        maxLines = 1,
+//        colors = TextFieldDefaults.out
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun CreditCardFormPreview() {
     AppTheme {
-        Greeting("Android")
+        CreditCardForm()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextFieldPreview() {
+    AppTheme {
+        TextField(placeholder = "Placeholder")
     }
 }
