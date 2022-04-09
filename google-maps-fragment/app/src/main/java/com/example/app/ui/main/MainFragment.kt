0dbd506a4f968ce69623ed96a81524303fb309eb
Natthawut Haematulin <natthawut1991@gmail.com>
2022-04-09 15:34:29 +0700

    feat: add sample google maps fragment

--- google-maps-fragment/app/src/main/java/com/example/app/ui/main/MainFragment.kt
+++ google-maps-fragment/app/src/main/java/com/example/app/ui/main/MainFragment.kt
@@ -1,13 +1,18 @@
-package com.example.app
+package com.example.app.ui.main
 
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.app.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -18,29 +23,37 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
+class MainFragment : Fragment(), OnMapReadyCallback {
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
     private var marker: Marker? = null
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var googleMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    companion object {
-        const val REQUEST_LOCATION_PERMISSION = 1
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync {
             this.googleMap = it
 
             enableMyLocation()
         }
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -50,20 +63,33 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         enableMyLocation()
     }
 
-    private fun enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-        ) {
+    private val permissionLocationLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            enableMyLocation()
+        }
+    }
 
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             googleMap.isMyLocationEnabled = true
 
             fusedLocationClient.lastLocation.addOnSuccessListener { location ->
                 // Got last known location. In some rare situations this can be null.
-                Log.i("MainActivity", "${location.latitude},${location.longitude}")
-                initialMarker(location)
+                Log.i("MainActivity", "${location?.latitude},${location?.longitude}")
+                location?.let { initialMarker(it) }
             }
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+            permissionLocationLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 
@@ -76,28 +102,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f))
         this.googleMap.apply {
             setOnCameraMoveListener {
-                val targetPosition = this@MainActivity.googleMap.cameraPosition.target
+                val targetPosition = this@MainFragment.googleMap.cameraPosition.target
                 marker?.position = targetPosition
             }
 
             setOnCameraIdleListener {
-                val targetPosition = this@MainActivity.googleMap.cameraPosition.target
+                val targetPosition = this@MainFragment.googleMap.cameraPosition.target
                 Log.i("MainActivity", "${targetPosition.latitude},${targetPosition.longitude}")
                 // TODO: Get maps location from API
             }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        // Check if location permissions are granted and if so enable the
-        // location data layer.
-        when (requestCode) {
-            REQUEST_LOCATION_PERMISSION ->
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    enableMyLocation()
-                }
         }
     }
 }
