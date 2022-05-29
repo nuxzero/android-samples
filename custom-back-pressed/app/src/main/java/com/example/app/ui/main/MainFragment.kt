08d04d86e6cdf097a2e5cf991a6ecbab9c51e20a
Natthawut Haematulin <natthawut1991@gmail.com>
2022-05-29 15:29:23 +0700

    feat: add sample custom back press app

---
+++ custom-back-pressed/app/src/main/java/com/example/app/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.app.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import com.example.app.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Toast.makeText(requireContext(), "On back pressed!", Toast.LENGTH_SHORT).show()
+//                requireActivity().onBackPressed()
+            }
+        })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
